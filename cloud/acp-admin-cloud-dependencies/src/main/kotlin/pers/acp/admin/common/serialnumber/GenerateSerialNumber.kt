bb4cd3dea15ab04191febd79e0a4835c1e4cb4f2
zhangbin1010 <391289241@qq.com>
2019-08-04 00:27:33 +0800

    [Upgrade] 公共模块中增加基于Redis的全局序列号生成组件 **pers.acp.admin.common.serialnumber.GenerateSerialNumber**

--- cloud/acp-admin-cloud-dependencies/src/main/kotlin/pers/acp/admin/common/serialnumber/GenerateSerialNumber.kt
+++ cloud/acp-admin-cloud-dependencies/src/main/kotlin/pers/acp/admin/common/serialnumber/GenerateSerialNumber.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.springframework.data.redis.core.RedisTemplate
 import pers.acp.core.CommonTools
+import java.util.concurrent.TimeUnit
 
 /**
  * @author zhang by 03/08/2019
@@ -14,22 +15,22 @@ class GenerateSerialNumber(private val redisTemplate: RedisTemplate<Any, Any>) {
     /**
      * 生成序列号
      * @param keyString 序列号键名称
-     * @param expirationTime 过期时间（重新计数超时时间）
+     * @param expirationTime 过期时间（重新计数超时时间），单位毫秒，默认24小时
      */
     @JvmOverloads
-    fun getSerialNumber(keyString: String = CommonTools.getNowString(), expirationTime: Long = 86400000): Long {
-        val result = redisTemplate.execute { connection ->
-            connection.execute("incr", keyString.toByteArray())
-        } as Long
-        if (result == 1L) {
-            GlobalScope.launch {
-                redisTemplate.execute { connection ->
-                    connection.execute("pexpire",
-                            keyString.toByteArray(),
-                            expirationTime.toString().toByteArray())
+    fun getSerialNumber(keyString: String = CommonTools.getNowString(), expirationTime: Long = 86400000): Long =
+            (redisTemplate.execute { connection ->
+                connection.execute("incr", keyString.toByteArray())
+            } as Long).also {
+                if (it == 1L) {
+                    GlobalScope.launch {
+                        redisTemplate.execute { connection ->
+                            connection.execute("pexpire",
+                                    keyString.toByteArray(),
+                                    expirationTime.toString().toByteArray())
+                        }
+                    }
                 }
             }
-        }
-        return result
-    }
+
 }
