ca15495daa8ba3b00bfbe7deda228f733d07d0f8
zhangbin1010 <391289241@qq.com>
2020-01-13 09:11:37 +0800

    [Upgrade] 修改feign客户端写法

--- cloud/acp-admin-cloud-dependencies/src/main/kotlin/pers/acp/admin/common/feign/WorkFlowServer.kt
+++ cloud/acp-admin-cloud-dependencies/src/main/kotlin/pers/acp/admin/common/feign/WorkFlowServer.kt
@@ -23,72 +23,63 @@ interface WorkFlowServer {
     /**
      * 启动流程
      */
-    @RequestMapping(value = [WorkFlowApi.basePath + WorkFlowApi.start], method = [RequestMethod.PUT],
-            produces = [MediaType.APPLICATION_JSON_VALUE])
+    @PutMapping(value = [WorkFlowApi.basePath + WorkFlowApi.start], produces = [MediaType.APPLICATION_JSON_VALUE])
     @Throws(ServerException::class)
     fun start(@RequestBody processStartPo: ProcessStartPo): InfoVo
 
     /**
      * 领取任务
      */
-    @RequestMapping(value = [WorkFlowApi.basePath + WorkFlowApi.claim + "/{taskId}"], method = [RequestMethod.PATCH],
-            produces = [MediaType.APPLICATION_JSON_VALUE])
+    @PatchMapping(value = [WorkFlowApi.basePath + WorkFlowApi.claim + "/{taskId}"], produces = [MediaType.APPLICATION_JSON_VALUE])
     @Throws(ServerException::class)
     fun claim(@PathVariable taskId: String): InfoVo
 
     /**
      * 转办任务
      */
-    @RequestMapping(value = [WorkFlowApi.basePath + WorkFlowApi.transfer + "/{taskId}/{userId}"], method = [RequestMethod.PATCH],
-            produces = [MediaType.APPLICATION_JSON_VALUE])
+    @PatchMapping(value = [WorkFlowApi.basePath + WorkFlowApi.transfer + "/{taskId}/{userId}"], produces = [MediaType.APPLICATION_JSON_VALUE])
     @Throws(ServerException::class)
     fun transfer(@PathVariable taskId: String, @PathVariable userId: String): InfoVo
 
     /**
      * 委托任务
      */
-    @RequestMapping(value = [WorkFlowApi.basePath + WorkFlowApi.delegate + "/{taskId}/{acceptUserId}"], method = [RequestMethod.PATCH],
-            produces = [MediaType.APPLICATION_JSON_VALUE])
+    @PatchMapping(value = [WorkFlowApi.basePath + WorkFlowApi.delegate + "/{taskId}/{acceptUserId}"], produces = [MediaType.APPLICATION_JSON_VALUE])
     @Throws(ServerException::class)
     fun delegate(@PathVariable taskId: String, @PathVariable acceptUserId: String): InfoVo
 
     /**
      * 流程处理
      */
-    @RequestMapping(value = [WorkFlowApi.basePath + WorkFlowApi.process], method = [RequestMethod.POST],
-            produces = [MediaType.APPLICATION_JSON_VALUE])
+    @PostMapping(value = [WorkFlowApi.basePath + WorkFlowApi.process], produces = [MediaType.APPLICATION_JSON_VALUE])
     @Throws(ServerException::class)
     fun process(@RequestBody processHandlingPo: ProcessHandlingPo): InfoVo
 
     /**
      * 强制终止流程实例
      */
-    @RequestMapping(value = [WorkFlowApi.basePath + WorkFlowApi.termination], method = [RequestMethod.POST],
-            produces = [MediaType.APPLICATION_JSON_VALUE])
+    @PostMapping(value = [WorkFlowApi.basePath + WorkFlowApi.termination], produces = [MediaType.APPLICATION_JSON_VALUE])
     @Throws(ServerException::class)
     fun termination(@RequestBody processTerminationPo: ProcessTerminationPo): InfoVo
 
     /**
      * 获取流程实例信息
      */
-    @RequestMapping(value = [WorkFlowApi.basePath + WorkFlowApi.instance + "/{processInstanceId}"], method = [RequestMethod.GET],
-            produces = [MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping(value = [WorkFlowApi.basePath + WorkFlowApi.instance + "/{processInstanceId}"], produces = [MediaType.APPLICATION_JSON_VALUE])
     @Throws(ServerException::class)
     fun getInstance(@PathVariable processInstanceId: String): ProcessInstanceVo?
 
     /**
      * 获取流程处理记录
      */
-    @RequestMapping(value = [WorkFlowApi.basePath + WorkFlowApi.history + "/{processInstanceId}"], method = [RequestMethod.GET],
-            produces = [MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping(value = [WorkFlowApi.basePath + WorkFlowApi.history + "/{processInstanceId}"], produces = [MediaType.APPLICATION_JSON_VALUE])
     @Throws(ServerException::class)
     fun getHistoryActivity(@PathVariable processInstanceId: String): MutableList<ProcessHistoryActivityVo>
 
     /**
      * 获取任务信息
      */
-    @RequestMapping(value = [WorkFlowApi.basePath + WorkFlowApi.task + "/{taskId}"], method = [RequestMethod.GET],
-            produces = [MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping(value = [WorkFlowApi.basePath + WorkFlowApi.task + "/{taskId}"], produces = [MediaType.APPLICATION_JSON_VALUE])
     @Throws(ServerException::class)
     fun getTaskInfo(@PathVariable taskId: String): ProcessTaskVo?
 }
