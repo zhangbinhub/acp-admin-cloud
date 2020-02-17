dd83bece93862e95ac8ea79aa422357663d20db6
zhangbin1010 <391289241@qq.com>
2020-02-17 12:21:27 +0800

    [Upgrade] 增加总线事件

---
+++ cloud/acp-admin-cloud-dependencies/src/main/kotlin/pers/acp/admin/common/event/ExecuteBusEvent.kt
@@ -0,0 +1,9 @@
+package pers.acp.admin.common.event
+
+import org.springframework.cloud.bus.event.RemoteApplicationEvent
+
+/**
+ * @author zhang by 19/03/2019
+ * @since JDK 11
+ */
+class ExecuteBusEvent(origin: String?, destination: String?, val executeType: String, val paramList: List<String>, source: Any = Object()) : RemoteApplicationEvent(source, origin, destination)
