35069285a5b20b80b256b5a590a811a37e7cc531
zhangbin1010 <391289241@qq.com>
2020-12-28 15:56:29 +0800

    [Upgrade] 调整oauth2相关client和user服务类

--- cloud/oauth-server/src/main/kotlin/pers/acp/admin/oauth/security/SecurityUserDetailsService.kt
+++ cloud/oauth-server/src/main/kotlin/pers/acp/admin/oauth/security/SecurityUserDetailsService.kt
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Component
 import pers.acp.admin.constant.RoleCode
+import pers.acp.admin.oauth.domain.ModuleFuncDomain
 import pers.acp.admin.oauth.domain.UserDomain
 import pers.acp.spring.boot.interfaces.LogAdapter
 
@@ -18,7 +19,11 @@ import pers.acp.spring.boot.interfaces.LogAdapter
  */
 @Component
 class SecurityUserDetailsService @Autowired
-constructor(private val logAdapter: LogAdapter, private val userDomain: UserDomain) : UserDetailsService {
+constructor(
+    private val logAdapter: LogAdapter,
+    private val userDomain: UserDomain,
+    private val moduleFuncDomain: ModuleFuncDomain
+) : UserDetailsService {
 
     /**
      * 根据 username 获取用户信息
@@ -28,19 +33,19 @@ constructor(private val logAdapter: LogAdapter, private val userDomain: UserDoma
      * @throws UsernameNotFoundException 找不到用户信息异常
      */
     @Throws(UsernameNotFoundException::class)
-    override fun loadUserByUsername(username: String): UserDetails {
-        val user = userDomain.getUserInfoByLoginNo(username)
-        if (user == null) {
-            logAdapter.error("无此用户：$username")
-            throw UsernameNotFoundException("无此用户：$username")
-        }
-        val grantedAuthorities: MutableSet<GrantedAuthority> = mutableSetOf()
-        user.roleSet.forEach { role ->
-            grantedAuthorities.add(SimpleGrantedAuthority(RoleCode.prefix + role.code)) //角色编码
-            role.moduleFuncSet.forEach { module ->
+    override fun loadUserByUsername(username: String): UserDetails =
+        userDomain.getUserInfoByLoginNo(username, true).let { user ->
+            if (user == null) {
+                logAdapter.error("无此用户：$username")
+                throw UsernameNotFoundException("无此用户：$username")
+            }
+            val grantedAuthorities: MutableSet<GrantedAuthority> = mutableSetOf()
+            user.roleSet.forEach { role ->
+                grantedAuthorities.add(SimpleGrantedAuthority(RoleCode.prefix + role.code)) //角色编码
+            }
+            moduleFuncDomain.getModuleFuncList(user.id).forEach { module ->
                 grantedAuthorities.add(SimpleGrantedAuthority(module.code)) //模块功能编码
             }
+            User(user.loginNo, user.password, user.enabled, true, true, true, grantedAuthorities)
         }
-        return User(user.loginNo, user.password, user.enabled, true, true, true, grantedAuthorities)
-    }
 }
