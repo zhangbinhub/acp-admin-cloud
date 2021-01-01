0f77be9ed538cdab6695e91e42ffb657820b78b1
zhangbin1010 <391289241@qq.com>
2021-01-02 01:12:40 +0800

    [Upgrade] 优化API文档

--- common/deploy-server/src/main/kotlin/pers/acp/admin/deploy/controller/DeployTaskController.kt
+++ common/deploy-server/src/main/kotlin/pers/acp/admin/deploy/controller/DeployTaskController.kt
@@ -39,34 +39,41 @@ import javax.validation.constraints.*
 @RequestMapping(DeployApi.basePath)
 @Api(tags = ["部署任务管理"])
 class DeployTaskController @Autowired
-constructor(logAdapter: LogAdapter,
-            private val objectMapper: ObjectMapper,
-            private val deployTaskDomain: DeployTaskDomain,
-            private val deployEventPublish: DeployEventPublish) : BaseController(logAdapter) {
+constructor(
+    logAdapter: LogAdapter,
+    private val objectMapper: ObjectMapper,
+    private val deployTaskDomain: DeployTaskDomain,
+    private val deployEventPublish: DeployEventPublish
+) : BaseController(logAdapter) {
     @ApiOperation(value = "新建部署任务")
-    @ApiResponses(ApiResponse(code = 201, message = "创建成功", response = DeployTask::class), ApiResponse(code = 400, message = "参数校验不通过；", response = ErrorVo::class))
+    @ApiResponses(
+        ApiResponse(code = 201, message = "创建成功", response = DeployTask::class),
+        ApiResponse(code = 400, message = "参数校验不通过；", response = ErrorVo::class)
+    )
     @PreAuthorize(BaseExpression.superOnly)
     @PutMapping(value = [DeployApi.task], produces = [MediaType.APPLICATION_JSON_VALUE])
     @AcpCloudDuplicateSubmission
     @Throws(ServerException::class)
     fun add(@RequestBody @Valid deployTaskPo: DeployTaskPo): ResponseEntity<DeployTask> =
-            deployTaskDomain.doCreate(deployTaskPo).let {
-                ResponseEntity.status(HttpStatus.CREATED).body(it)
-            }
+        deployTaskDomain.doCreate(deployTaskPo).let {
+            ResponseEntity.status(HttpStatus.CREATED).body(it)
+        }
 
     @ApiOperation(value = "删除指定的部署任务")
     @ApiResponses(ApiResponse(code = 400, message = "参数校验不通过；", response = ErrorVo::class))
     @PreAuthorize(BaseExpression.superOnly)
     @DeleteMapping(value = [DeployApi.task], produces = [MediaType.APPLICATION_JSON_VALUE])
     @Throws(ServerException::class)
-    fun delete(@ApiParam(value = "id列表", required = true)
-               @NotEmpty(message = "id不能为空")
-               @NotNull(message = "id不能为空")
-               @RequestBody
-               idList: MutableList<String>): ResponseEntity<InfoVo> =
-            deployTaskDomain.doDelete(idList).let {
-                ResponseEntity.ok(InfoVo(message = "删除成功"))
-            }
+    fun delete(
+        @ApiParam(value = "id列表", required = true)
+        @NotEmpty(message = "id不能为空")
+        @NotNull(message = "id不能为空")
+        @RequestBody
+        idList: MutableList<String>
+    ): ResponseEntity<InfoVo> =
+        deployTaskDomain.doDelete(idList).let {
+            ResponseEntity.ok(InfoVo(message = "删除成功"))
+        }
 
     @ApiOperation(value = "更新指定的部署任务")
     @ApiResponses(ApiResponse(code = 400, message = "参数校验不通过；ID不能为空；找不到信息；", response = ErrorVo::class))
@@ -89,15 +96,15 @@ constructor(logAdapter: LogAdapter,
     @PostMapping(value = [DeployApi.task], produces = [MediaType.APPLICATION_JSON_VALUE])
     @Throws(ServerException::class)
     fun query(@RequestBody @Valid deployTaskQueryPo: DeployTaskQueryPo): ResponseEntity<Page<DeployTask>> =
-            deployTaskQueryPo.let {
-                if (it.startTime != null) {
-                    it.startTime = longToDate(it.startTime!!).millis
-                }
-                if (it.endTime != null) {
-                    it.endTime = longToDate(it.endTime!!).millis
-                }
-                ResponseEntity.ok(deployTaskDomain.doQuery(deployTaskQueryPo))
+        deployTaskQueryPo.let {
+            if (it.startTime != null) {
+                it.startTime = longToDate(it.startTime!!).millis
             }
+            if (it.endTime != null) {
+                it.endTime = longToDate(it.endTime!!).millis
+            }
+            ResponseEntity.ok(deployTaskDomain.doQuery(deployTaskQueryPo))
+        }
 
     @ApiOperation(value = "查询服务器文件")
     @PreAuthorize(BaseExpression.superOnly)
@@ -110,7 +117,10 @@ constructor(logAdapter: LogAdapter,
     @PreAuthorize(BaseExpression.superOnly)
     @PostMapping(value = [DeployApi.file], produces = [MediaType.APPLICATION_JSON_VALUE])
     @Throws(ServerException::class)
-    fun uploadFile(@RequestParam file: MultipartFile): ResponseEntity<InfoVo> {
+    fun uploadFile(
+        @RequestPart(value = "文件", required = true)
+        @RequestParam file: MultipartFile
+    ): ResponseEntity<InfoVo> {
         if (file.isEmpty) {
             throw ServerException("请选择需要上传的文件")
         }
@@ -124,10 +134,15 @@ constructor(logAdapter: LogAdapter,
     @PreAuthorize(BaseExpression.superOnly)
     @DeleteMapping(value = [DeployApi.file], produces = [MediaType.APPLICATION_JSON_VALUE])
     @Throws(ServerException::class)
-    fun deleteFile(@ApiParam(value = "文件名称", required = true)
-                   @NotBlank(message = "文件名称不能为空")
-                   @RequestParam fileName: String): ResponseEntity<InfoVo> {
-        val name = String(Base64.decode(fileName), Charset.forName(CommonTools.getDefaultCharset())).replace("/", File.separator).replace("\\", File.separator)
+    fun deleteFile(
+        @ApiParam(value = "文件名称", required = true)
+        @NotBlank(message = "文件名称不能为空")
+        @RequestParam fileName: String
+    ): ResponseEntity<InfoVo> {
+        val name = String(Base64.decode(fileName), Charset.forName(CommonTools.getDefaultCharset())).replace(
+            "/",
+            File.separator
+        ).replace("\\", File.separator)
         return deployTaskDomain.deleteFile(name).let {
             ResponseEntity.ok(InfoVo(message = "文件删除成功"))
         }
@@ -137,15 +152,21 @@ constructor(logAdapter: LogAdapter,
     @ApiResponses(ApiResponse(code = 400, message = "参数校验不通过；", response = ErrorVo::class))
     @GetMapping(value = [DeployApi.fileDownLoad], produces = [MediaType.ALL_VALUE])
     @Throws(ServerException::class)
-    fun downloadFile(request: HttpServletRequest, response: HttpServletResponse,
-                     @ApiParam(value = "文件内容", required = true)
-                     @NotBlank(message = "文件内容不能为空")
-                     @RequestParam params: String) {
+    fun downloadFile(
+        request: HttpServletRequest, response: HttpServletResponse,
+        @ApiParam(value = "文件内容", required = true)
+        @NotBlank(message = "文件内容不能为空")
+        @RequestParam params: String
+    ) {
         val fileContent = objectMapper.readValue(params, FileDownLoadPo::class.java)
         if (CommonTools.isNullStr(fileContent.fileName)) {
             throw ServerException("文件名称不能为空")
         }
-        val fileName = String(Base64.decode(fileContent.fileName), Charset.forName(CommonTools.getDefaultCharset())).replace("/", File.separator).replace("\\", File.separator)
+        val fileName =
+            String(Base64.decode(fileContent.fileName), Charset.forName(CommonTools.getDefaultCharset())).replace(
+                "/",
+                File.separator
+            ).replace("\\", File.separator)
         deployTaskDomain.doDownLoadFile(request, response, fileName)
     }
 
@@ -155,9 +176,9 @@ constructor(logAdapter: LogAdapter,
     @PostMapping(value = [DeployApi.taskExecute + "/{id}"], produces = [MediaType.APPLICATION_JSON_VALUE])
     @Throws(ServerException::class)
     fun execute(@PathVariable id: String): ResponseEntity<InfoVo> =
-            deployTaskDomain.executeTask(id).let { deployTask ->
-                deployEventPublish.doNotifyExecuteDeploy(deployTask.id).let {
-                    ResponseEntity.accepted().body(InfoVo(message = "请求成功，稍后将执行部署任务"))
-                }
+        deployTaskDomain.executeTask(id).let { deployTask ->
+            deployEventPublish.doNotifyExecuteDeploy(deployTask.id).let {
+                ResponseEntity.accepted().body(InfoVo(message = "请求成功，稍后将执行部署任务"))
             }
+        }
 }
