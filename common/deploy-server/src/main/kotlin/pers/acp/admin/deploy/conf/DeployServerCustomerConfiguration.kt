75336aa410ce3bfc2aa5a51f83db3edcd74e5271
zhangbin1010 <391289241@qq.com>
2021-03-24 17:46:43 +0800

    [Upgrade] acp 6.5.4

--- common/deploy-server/src/main/kotlin/pers/acp/admin/deploy/conf/DeployServerCustomerConfiguration.kt
+++ common/deploy-server/src/main/kotlin/pers/acp/admin/deploy/conf/DeployServerCustomerConfiguration.kt
@@ -1,8 +1,10 @@
 package pers.acp.admin.deploy.conf
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.stereotype.Component
+import pers.acp.spring.cloud.component.CloudTools
 
 /**
  * @author zhang by 30/01/2019
@@ -10,12 +12,10 @@ import org.springframework.stereotype.Component
  */
 @Component
 @RefreshScope
-class DeployServerCustomerConfiguration {
-    @Value("\${server.address}")
-    var serverIp: String = ""
+class DeployServerCustomerConfiguration @Autowired constructor(cloudTools: CloudTools) {
+    var serverIp: String = cloudTools.getServerIp()
 
-    @Value("\${server.port}")
-    var serverPort: Int = 0
+    var serverPort: Int = cloudTools.getServerPort()
 
     @Value("\${deploy-server.upload-path}")
     var uploadPath: String = ""
