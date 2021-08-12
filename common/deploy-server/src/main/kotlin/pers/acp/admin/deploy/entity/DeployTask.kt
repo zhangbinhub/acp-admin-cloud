dcb45303bf236e1dda5a2dc0347f3ebe537716ae
zhangbin1010 <391289241@qq.com>
2021-08-12 15:10:33 +0800

    [Upgrade] 部署任务增加参数支持：提供参数文件

--- common/deploy-server/src/main/kotlin/pers/acp/admin/deploy/entity/DeployTask.kt
+++ common/deploy-server/src/main/kotlin/pers/acp/admin/deploy/entity/DeployTask.kt
@@ -9,45 +9,48 @@ import javax.persistence.*
 @Table(name = "t_deploy")
 @ApiModel("部署任务")
 data class DeployTask(
-        @Id
-        @GenericGenerator(name = "idGenerator", strategy = "uuid")
-        @GeneratedValue(generator = "idGenerator")
-        @Column(length = 36, nullable = false)
-        @ApiModelProperty("ID")
-        var id: String = "",
+    @Id
+    @GenericGenerator(name = "idGenerator", strategy = "uuid")
+    @GeneratedValue(generator = "idGenerator")
+    @Column(length = 36, nullable = false)
+    @ApiModelProperty("ID")
+    var id: String = "",
 
-        @Column(nullable = false)
-        @ApiModelProperty("任务名称")
-        var name: String = "",
+    @Column(nullable = false)
+    @ApiModelProperty("任务名称")
+    var name: String = "",
 
-        @Column(nullable = false)
-        @ApiModelProperty("脚本文件")
-        var scriptFile: String = "",
+    @Column(nullable = false)
+    @ApiModelProperty("脚本文件")
+    var scriptFile: String = "",
 
-        @ApiModelProperty("执行的服务器IP正则表达式")
-        var serverIpRegex: String? = null,
+    @ApiModelProperty("参数文件")
+    var paramFile: String? = null,
 
-        @ApiModelProperty("备注")
-        var remarks: String? = null,
+    @ApiModelProperty("执行的服务器IP正则表达式")
+    var serverIpRegex: String? = null,
 
-        @Column(nullable = false)
-        @ApiModelProperty("创建人账号")
-        var createLoginNo: String = "",
+    @ApiModelProperty("备注")
+    var remarks: String? = null,
 
-        @Column(nullable = false)
-        @ApiModelProperty("创建人名称")
-        var createUserName: String = "",
+    @Column(nullable = false)
+    @ApiModelProperty("创建人账号")
+    var createLoginNo: String = "",
 
-        @Column(nullable = false)
-        @ApiModelProperty("创建时间")
-        var createTime: Long = 0,
+    @Column(nullable = false)
+    @ApiModelProperty("创建人名称")
+    var createUserName: String = "",
 
-        @ApiModelProperty("执行人账号")
-        var execLoginNo: String? = null,
+    @Column(nullable = false)
+    @ApiModelProperty("创建时间")
+    var createTime: Long = 0,
 
-        @ApiModelProperty("执行人名称")
-        var execUserName: String? = null,
+    @ApiModelProperty("执行人账号")
+    var execLoginNo: String? = null,
 
-        @ApiModelProperty("执行时间")
-        var execTime: Long? = null
+    @ApiModelProperty("执行人名称")
+    var execUserName: String? = null,
+
+    @ApiModelProperty("执行时间")
+    var execTime: Long? = null
 )
